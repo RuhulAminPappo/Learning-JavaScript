9d264afc3c37f1fe066b9364fa9481060abfc495
Ruhul Amin Pappo <154083059+RuhulAminPappo@users.noreply.github.com>
2025-03-15 01:31:23 +0600

    Add files via upload

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//shift + option + down arrow = copy and paste below
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
